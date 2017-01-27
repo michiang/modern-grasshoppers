9f588b4e84b59433ebbe8263c2446f428d2b6231
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-01-27 13:49:08 -0600

    Fixed git ignore, set up server and example routes

---
+++ server/config/routes.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// database connection
+
+// Do we need this?
+// app.use(express.static('client'));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.sendFile('../../client/public/index.html');
+  // prompt('Enter your username:');
+});
+
+app.get('/users/tasks', function(req, res) {
+  // get all tasks from db
+});
+
+app.post('/users/task', function(req, res) {
+  // post new task to db
+});
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,7 @@
+var app = require('./config/routes.js');
+
+var port = process.env.PORT || 1337;
+
+app.listen(port, function() {
+  console.log('Server now listening on port: ' + port);
+});
