8fa5ffd0f0f30078417ae3ef2b8aa7626b7cd6a9
gwynndp <gwynndp@gmail.com>
2017-01-27 11:44:56 -0800

    Make .gitignore active

---
+++ src/app.js
@@ -0,0 +1,17 @@
+"use strict";
+import React from 'react'
+import ReactDOM from 'react-dom'
+ReactDOM.render(
+  <h1>You Really Did It!!</h1>,
+  document.getElementById('app')
+)
+
+
+// task list
+
+// task list entry
+
+// start button
+
+// stop button
+
