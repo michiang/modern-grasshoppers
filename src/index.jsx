b8e6edb2d0c9446b0a2f3994bf8de4bae82a7374
gwynndp <gwynndp@gmail.com>
2017-01-29 16:06:01 -0800

    Front-end connected to server

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,10 @@ var ReactDOM = require('react-dom');
 
 console.log('INSIDE INDEX.JS');
 
-//ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
-// test
-ReactDOM.render(
-  <h1>You Really Did It!!</h1>,
-  document.getElementById('app')
-);
+// test works
+// ReactDOM.render(
+//   <h1>You Really Did It!!</h1>,
+//   document.getElementById('app')
+// );
