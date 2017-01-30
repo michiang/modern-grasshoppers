9ea64e1e9e0e3a311ce952bc6f51d816587d47f6
Michael Chiang <michiang@ucsd.edu>
2017-01-30 12:49:02 -0800

    Implemented front-end GET and POST ajax requests, set up event handlers, started implementation on start and stop, rendering stateless components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-var React = require('react');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class App extends React.Component {
   constructor (props) {
@@ -6,10 +8,54 @@ class App extends React.Component {
 
     this.state = {
       tasks: tasks,
+      body: '',
       start: false,
       stop: false
     }
+
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleChange = this.handleChange.bind(this);
   }
+
+  //Ajax get request needs to be wrapped in a function
+  //so that the request can be called every time
+  //componentdidMount is invoked
+  //Loads data from API
+  loadDataFromServer() {
+    $.get('/users/tasks', function(data) {
+      console.log(data);
+      global.allData = data;
+    });
+  }
+
+  //Post data to the server only when the stop button event handler
+  //is triggered
+  postDataToServer() {
+    $("StopButton").click(function() {
+      $.post('users/tasks'),
+      {
+        task: $("StopButton").val(),
+        timer: "time"
+      },
+      function (data) {
+        console.log(data);
+      }
+    })
+  })
+}
+
+handleChange(event) {
+  this.setState({task: event.target.value});
+}
+
+handleSubmit(e) {
+  e.preventDefault()
+  //this.setState({body: })
+}
+
+componentdidMount() {
+  this.loadDataFromServer();
+}
 
   render() {
     return(
@@ -21,6 +67,10 @@ class App extends React.Component {
           <TaskList
             tasks={this.state.tasks}
           />
+        </div>
+        <div className='buttons'>
+          <StartButton onStartButtonClick = {this.onStartButtonClick.bind(this)}/>
+          <StopButton onStopButtonClick = {this.onStopButtonClick.bind(this)}/>
         </div>
       </div>
     );
