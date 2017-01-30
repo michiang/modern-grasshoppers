f7bb33aec499f871d3f8ef87f73ac1d4917635e1
Michael Chiang <michiang@ucsd.edu>
2017-01-30 12:06:59 -0800

    Started startbutton implementation

---
+++ src/components/StartButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const StartButton = (props) => {
+  return (
+    <button className = "startbutton"></button>
+      <div className="media-left media-middle"></div>
+
+}
+
+window.StartButton = StartButton;
