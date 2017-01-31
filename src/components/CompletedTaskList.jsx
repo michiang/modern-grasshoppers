7e0a0a51523101619b37bbfadf43b0c7fe2f194e
Michael Chiang <michiang@ucsd.edu>
2017-01-31 10:18:44 -0800

    Renamed taskList/taskListEntry to completed tasks

--- src/components/CompletedTaskList.jsx
+++ src/components/CompletedTaskList.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 
-var TaskList = (props) => {
+var CompletedTaskList = (props) => {
   return (
     <div className="task list">
     {props.tasks.map((task) =>
@@ -17,10 +17,10 @@ var TaskList = (props) => {
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 
-TaskList.propTypes = {
+CompletedTaskList.propTypes = {
   tasks: React.PropTypes.array.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
-window.TaskList = TaskList;
+window.CompletedTaskList = CompletedTaskList;
