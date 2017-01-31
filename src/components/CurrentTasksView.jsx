39ed89fb420ee974bcd45ec104d1bd45daf7836e
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-01-31 12:57:38 -0600

    Create CurrentTasks/View components

---
+++ src/components/CurrentTasksView.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var TaskList = (props) => {
+  return (
+    <div className="task list">
+    {props.tasks.map((task) =>
+      <TaskListItems
+        task={task.task}
+        time={task.total_time}
+        //key={task.id}
+      />
+    )}
+    </div>
+  );
+}
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+
+TaskList.propTypes = {
+  tasks: React.PropTypes.array.isRequired
+};
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope.
+// `var` declarations will only exist globally where explicitly defined.
+window.TaskList = TaskList;
