10be1834a4c848152ff988ce9e8a112022648f9e
gwynndp <gwynndp@gmail.com>
2017-02-03 14:22:11 -0800

    Routes mostly working with authorization
    Can sign in
    Can see user's tasks
    Need to click link to see tasks, it's not automatically redirecting
    Need to make authorization recognize cookie, it's lost` w/ page refresh

--- src/components/CheckLoggedIn.jsx
+++ src/components/CheckLoggedIn.jsx
@@ -2,29 +2,58 @@ import React from 'react';
 import $ from 'jquery';
 
 class CheckLoggedIn extends React.Component {
-  constructor (props) {
-    super(props);
+  // constructor (props) {
+  //   super(props);
 
-    this.state = {
-      isAuth: false
-    }
-  }
+  //   this.state = {
+  //     isAuth: false
+  //   }
+  // }
 
   componentDidMount() {
-    this.isAuth = requireAuth();
+    //this.props.isLoggedIn = requireAuth();
+    var isAuth = requireAuth();
+    console.log('this.props.isLoggedIn', this.props.isLoggedIn);
 
-    if (!this.isAuth) {
-      console.log('CheckLoggedIn = false', this.isAuth);
-      this.props.history.replace('/signin');
+    if (!this.props.isLoggedIn) {
+      console.log('CheckLoggedIn = false', this.props.isLoggedIn, isAuth);
+      this.props.router.replace('/signin');
       //browserHistory.replace("/signin")
+    } else {
+      console.log('CheckLoggedIn = true', this.props.isLoggedIn, isAuth);
+      this.props.router.replace('/tasks');
     }
   }
 
   render() {
-    if (this.isAuth) {
-      console.log('CheckLoggedIn = true', isAuth);
+    if (this.props.isLoggedIn) {
+      console.log('CheckLoggedIn = true', this.props.isLoggedIn);
       //this.props.router.replace('/tasks');
-      return this.props.children
+      return (
+        this.props.children && React.cloneElement(this.props.children, {
+              postDataToServer: this.props.postDataToServer,
+              onStartButtonClick: this.props.onStartButtonClick,
+              onStopButtonClick: this.props.onStopButtonClick,
+              handleChange: this.props.handleChange,
+              handleSubmit: this.props.handleSubmit,
+              handleUsernameChange: this.props.handleUsernameChange,
+              postToSignin: this.props.postToSignin,
+              postToSignup: this.props.postToSignup,
+              signout: this.props.signout,
+              loadDataFromServer: this.props.loadDataFromServer,
+              tasks: this.props.tasks,
+              currentTask: this.props.currentTask,
+              currentTaskArray: this.props.currentTaskArray,
+              start_time: this.props.start_time,
+              started: this.props.started,
+              passwordInSignin: this.props.passwordInSignin,
+              usernameInSignin: this.props.usernameInSignin,
+              usernameInSignup: this.props.usernameInSignup,
+              passwordInSignup: this.props.passwordInSignup,
+              currentUser: this.props.currentuser,
+              isLoggedIn: this.props.isLoggedIn
+            })
+      );
     } else {
       return null
     }
@@ -36,22 +65,21 @@ function requireAuth () {
     var isAuth;
     //var that = App;
     $.ajax({
-      type: "POST",
+      type: 'GET',
       url: '/tasks',
       success: function(data) {
-        console.log('POST AUTH SUCCESS', data);
         isAuth = true;
+        console.log('POST AUTH SUCCESS', isAuth);
       },
       error: function(error) {
         //console.log('SIGN-IN FAILURE STATE', that.state);
-        console.log('POST AUTH OOPS!', error);
         isAuth = false;
+        console.log('POST AUTH OOPS!', isAuth);
       },
       contentType: 'application/json',
       dataType: 'json'
     });
 
-    App.isLoggedIn = isAuth;
     return isAuth;
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import $ from 'jquery';
 //import routes from './components/routes.jsx'
 import CheckLoggedIn from './components/CheckLoggedIn.jsx'
 //import App from './components/App.jsx'
@@ -17,7 +18,10 @@ var app = document.getElementById('app');
 
 //       <Route path="signin" component={UserSignIn} />
 //       <Route path="signup" component={UserSignUp} />
-//       <Route path='tasks' component={Layout} onEnter={requireAuth} />
+//        <Route path="layout" component={Layout} onEnter={requireAuth} />
+//       <Route component={CheckLoggedIn}>
+//         <Route path="layout" component={Layout} />
+//       </Route>
 //       <Route path="layout" component={NamedComponents}>
 //         <IndexRoute components={{ taskEntry: TaskEntry, currentTasks: CurrentTasks, completedTaskList: CompletedTaskList }} />
 //       </Route>
@@ -32,44 +36,91 @@ var routes = (
       <Route path="signin" component={UserSignIn} />
       <Route path="signup" component={UserSignUp} />
       <Route component={CheckLoggedIn}>
-        <Route path="layout" component={Layout} />
+        <Route path="tasks" component={Layout} />
       </Route>
-      <Route path="tasks" component={NamedComponents}>
-        <IndexRoute components={{ taskEntry: TaskEntry, currentTasks: CurrentTasks, completedTaskList: CompletedTaskList }} />
-      </Route>
-
       <Route path='*' component={NotFound} />
     </Route>
 );
 
 const NotFound = () => (
-  <h1>404.. This page is not found!</h1>)
+  <h1>404.. This page is not found!</h1>
+)
 
-const NamedComponents = (props) => (
-  <div>
-    {this.props.children}
-    {this.props.children.taskEntry}<br />
-    {this.props.children.currentTasks}<br />
-    {this.props.children.completedTaskList}
-  </div>
-);
+// const NamedComponents = (props) => (
+//   <div>
+//     {this.props.children && React.cloneElement(this.props.children, {
+//               postDataToServer: this.postDataToServer.bind(this),
+//               onStartButtonClick: this.onStartButtonClick.bind(this),
+//               onStopButtonClick: this.onStopButtonClick.bind(this),
+//               handleChange: this.handleChange.bind(this),
+//               handleSubmit: this.handleSubmit.bind(this),
+//               handleUsernameChange: this.handleUsernameChange.bind(this),
+//               postToSignin: this.postToSignin.bind(this),
+//               postToSignup: this.postToSignup.bind(this),
+//               signout: this.signout.bind(this),
+//               loadDataFromServer: this.loadDataFromServer.bind(this),
+//               tasks: this.state.tasks,
+//               currentTask: this.state.currentTask,
+//               currentTaskArray: this.state.currentTaskArray,
+//               start_time: this.state.start_time,
+//               started: this.state.started,
+//               passwordInSignin: this.state.passwordInSignin,
+//               usernameInSignin: this.state.usernameInSignin,
+//               usernameInSignup: this.state.usernameInSignup,
+//               passwordInSignup: this.state.passwordInSignup,
+//               currentUser: this.state.currentuser,
+//               isLoggedIn: this.state.isLoggedIn
+//             })}
+//     {this.props.children.taskEntry}<br />
+//     {this.props.children.currentTasks}<br />
+//     {this.props.children.completedTaskList}
+//   </div>
+// );
 
-function createElement(Component, props) {
-    console.log('INSIDE CREATE ELEMENT', Component, props);
-   // Add myprop to props for all route components
-   // _.extend(props, {myprop: "foo"})
-  return <Component {...props}/>
-}
+// function createElement(Component, props) {
+//     console.log('INSIDE CREATE ELEMENT', Component, props);
+//    // Add myprop to props for all route components
+//    // _.extend(props, {myprop: "foo"})
+//   return <Component {...props}/>
+// }
 
-function requireAuth(nextState, replace, callback) {
-  console.log('requireAuth isLoggedIn', App.isLoggedIn);
-  if (App.isLoggedIn === undefined || App.isLoggedIn === false) {
-    replace({
-      pathname: '/signin',
-      state: { nextPathname: nextState.location.pathname }
-    })
-  }
-}
+// function requireAuth (nextState, replace, callback) {
+//     console.log('INSIDE REQUIRE AUTH', nextState, callback);
+//     var isAuth;
+//     //var that = App;
+//     $.ajax({
+//       type: 'GET',
+//       url: '/tasks',
+//       success: function(data) {
+//         isAuth = true;
+//         if (isAuth) {
+//           console.log('AUTH SUCCESS', isAuth, data);
+//           replace('/layout');
+//           callback();
+//         }
+//       },
+//       error: function(error) {
+//         //console.log('SIGN-IN FAILURE STATE', that.state);
+//         isAuth = false;
+//         if (!isAuth) {
+//           replace('/signin');
+//           console.log('AUTH OOPS!', isAuth, error, replace);
+//         }
+//       },
+//       contentType: 'application/json',
+//       dataType: 'json'
+//     });
+// }
+
+// function requireAuth(nextState, replace, callback) {
+//   console.log('requireAuth isLoggedIn', App.isLoggedIn);
+//   if (App.isLoggedIn === undefined || App.isLoggedIn === false) {
+//     replace({
+//       pathname: '/signin',
+//       state: { nextPathname: nextState.location.pathname }
+//     })
+//   }
+// }
 
 ReactDOM.render(
   <Router history={hashHistory}>{routes}</Router>,
