f41f9772a8c0d6ea63ec6d6a9a591ef7f4869e8a
gwynndp <gwynndp@gmail.com>
2017-02-03 23:39:07 -0800

    Clean up functions and make more consistent
    Sort out some of the timer functions and database storage logic
    Clear up confusing names, like currentTask is now activeTask for the newest
    and currentTask is now a boolean for whether the task should be in the currentTaskArray or not
    Added logic to sort the tasks data into two arrays, current and completed tasks
    when the data is loaded

--- src/components/PauseButton.jsx
+++ src/components/PauseButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 var PauseButton = (props) => {
   return (
-    <div className="start-time">
+    <div className="pause timer btn">
       <button className = "btn btn-default" onClick={props.onPauseButtonClick} > PAUSE </button>
     </div>
   );
