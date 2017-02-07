2e15b61546742425f278d960b8342518512d19a7
Gwynn Dandridge-Perry <gwynndp@gmail.com>
2017-02-06 20:14:41 -0800

    Delete config_example.js

--- server/config/config_example.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-//var databasepath = 'mongodb://localhost/whathaveidone';
-var databasepath = '';
-mongoose.connect(databasepath);
-
-var db = mongoose.connection;
-
-module.exports = db;
